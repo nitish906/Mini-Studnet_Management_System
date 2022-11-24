02ac45cb6073d34ef3c81c1330fac835d66765ef
nitish906 <nk08522015@gmail.com>
2022-11-24 13:48:18 +0530

    student

---
+++ Studnet_Management_System/src/main/java/com/masai/StudnetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudnetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudnetManagementSystemApplication.class, args);
+	}
+
+}
